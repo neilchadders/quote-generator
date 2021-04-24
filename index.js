f8d9e6b70baea00ce04f07e3f421bcd70aab3671
Neil Chadwick <neilchadders@hotmail.com>
2021-04-24 06:36:35 +0100

    add console.log

---
+++ index.js
@@ -0,0 +1,20 @@
+let apiQuotes = [];
+
+async function getQuotes(){
+	const apiUrl = "https://type.fit/api/quotes";
+try{
+	const response = await fetch(apiUrl);
+	apiQuotes = await fetch(apiUrl);
+	apiQuotes = await response.json();
+	console.log(apiQuotes);
+} catch (error) {
+
+}
+
+
+
+}
+
+getQuotes();
+
+
