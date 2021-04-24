1c9564435c3314de2f240473c12d4ee87a096586
Neil Chadwick <neilchadders@hotmail.com>
2021-04-24 17:47:53 +0100

    amend font-size

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
+
+
+
+//Show new quote
+
+function newQuote(){
+	// Pick a random quote from api
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]; 
+	authorText.textContent = quote.author;
+	quoteText.textContent = quote.text;
+
+}
+
+
+
+//Quote from API
 
 async function getQuotes(){
 	const apiUrl = "https://type.fit/api/quotes";
@@ -6,13 +28,10 @@ try{
 	const response = await fetch(apiUrl);
 	apiQuotes = await fetch(apiUrl);
 	apiQuotes = await response.json();
-	console.log(apiQuotes);
+	newQuote();
 } catch (error) {
 
-}
-
-
-
+	}
 }
 
 getQuotes();
